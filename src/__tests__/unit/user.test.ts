a6aae79c0fd82e2bacd46159e312d1c84993a79c
Matheus F Nascimento <matheusferreira_mfn@hotmail.com>
2021-09-22 17:39:10 -0300

    unit tests do login de user/admin

---
+++ src/__tests__/unit/user.test.ts
@@ -0,0 +1,77 @@
+require('dotenv').config();
+import jwt from "jsonwebtoken";
+import DBConnection from "../../database/DBConnection";
+import Admin from "../../models/Admin";
+import User from "../../models/User";
+import adminService from "../../services/admin.service";
+import userService from "../../services/user.service";
+import ErrorMessage from "../../utils/ErrorMessage";
+
+beforeAll(async () => {
+  await DBConnection.create();
+});
+
+afterAll(async () => {
+  await DBConnection.close();
+});
+
+describe('testing user service', () => {
+  const user = new User();
+  user.name = "Testinho Testículo";
+  user.email = "teste@teste.com";
+  user.password = "teste";
+
+  it('should create a user', async () => { 
+    const result = await userService.create(user) as User;
+    user.password = "teste"; 
+
+    expect(result instanceof User).toBeTruthy();
+    expect(result).toHaveProperty("id");
+    expect(result.name).toEqual(user.name);
+  });
+
+  it('should log in and receive a jwt', async () => {
+    const { email, password } = user;
+    const result = await userService.login(email, password) as string;
+    const decodedJwt = jwt.verify(result, process.env.SECRET || 'secret') as User;
+    expect(decodedJwt.id).toBe(user.id);
+  })
+
+  it('shouldn\'t log in (wrong password)', async () => {
+    const { email } = user;
+    const result = await userService.login(email, "senhaerrada") as ErrorMessage;
+    expect(result instanceof ErrorMessage).toBeTruthy();
+  });
+
+  it('shouldn\'t log in (wrong email)', async () => {
+    const { password } = user;
+    const result = await userService.login("emailerrado@errado.com", password) as ErrorMessage;
+    expect(result instanceof ErrorMessage).toBeTruthy();
+  });
+});
+
+describe('testing admin service', () => {
+  const superadmin = new Admin();
+  superadmin.password = process.env.DEFAULT_USER_PASSWORD || "password";
+  superadmin.email = process.env.DEFAULT_USER_EMAIL ?? "email";
+
+  it('should log in with default superadmin and receive a jwt', async () => { 
+    const { email, password } = superadmin;
+    const result = await adminService.login(email, password) as string;
+    const decodedJwt = jwt.verify(result, process.env.SECRET || 'secret') as Admin;
+    expect(decodedJwt).toHaveProperty("id");
+    expect(decodedJwt.role).toBe(7);
+  });
+
+  it('shouldn\'t log in as admin (wrong email)', async () => {
+    const { password } = superadmin;
+    const result = await userService.login("emailerrado@errado.com", password) as ErrorMessage;
+    expect(result instanceof ErrorMessage).toBeTruthy();
+  });
+
+  it('shouldn\'t log in as admin (wrong password)', async () => {
+    const { email } = superadmin;
+    const result = await userService.login(email, "senhaerrad@") as ErrorMessage;
+    expect(result instanceof ErrorMessage).toBeTruthy();
+  });
+});
