59a59bc85fa08189d2ef2f0a02e987b95b25f34c
Matheus F Nascimento <matheusferreira_mfn@hotmail.com>
2022-08-14 19:41:07 -0300

    default server port changed

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,6 @@ import DBConnection from './database/DBConnection';
 DBConnection.create();
 
 import app from './app';
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3333;
 
 app.listen(PORT, () => console.log(`Server connected on port ${PORT}`));
